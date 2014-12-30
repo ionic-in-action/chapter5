9703a0e7eca195bb5ef91ebd5c480cd2fca9f9f4
Jeremy Wilken <gnomation@gnomeontherun.com>
2014-12-30 16:33:54 -0600

    add individual tab views

--- www/js/app.js
+++ www/js/app.js
@@ -5,10 +5,35 @@ angular.module('App', ['ionic'])
   $stateProvider
     .state('tabs', {
       url: '/tabs',
+      abstract: true,
       templateUrl: 'views/tabs/tabs.html'
+    })
+    .state('tabs.rates', {
+      url: '/rates',
+      views: {
+        'rates-tab': {
+          templateUrl: 'views/rates/rates.html'
+        }
+      }
+    })
+    .state('tabs.history', {
+      url: '/history',
+      views: {
+        'history-tab': {
+          templateUrl: 'views/history/history.html'
+        }
+      }
+    })
+    .state('tabs.currencies', {
+      url: '/currencies',
+      views: {
+        'currencies-tab': {
+          templateUrl: 'views/currencies/currencies.html'
+        }
+      }
     });
 
-  $urlRouterProvider.otherwise('/tabs');
+  $urlRouterProvider.otherwise('/tabs/rates');
 })
 
 .run(function($ionicPlatform) {
